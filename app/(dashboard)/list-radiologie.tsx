71a67ddb68316b0a4e23dd284d418882869e0503
chaimae <chaimae.michich@ebitda.tech>
2024-12-08 16:08:17 +0100

    refine radiological exams design

--- app/(dashboard)/list-radiologie.tsx
+++ app/(dashboard)/list-radiologie.tsx
@@ -74,7 +74,7 @@ export default function ListRadiologie() {
     };
 
     return (
-        <SafeAreaView className="flex-1 bg-gray-50" style={{ position: 'relative' }}>
+        <SafeAreaView className="flex-1 bg-white" style={{ position: 'relative' }}>
             {/* Header */}
             <View className="bg-white px-4 pt-14 pb-4">
                 <View className="flex-row items-center justify-between mb-4">
@@ -109,43 +109,43 @@ export default function ListRadiologie() {
             <ScrollView className="flex-1 px-4 pt-4">
                 {radiologies.map((radio: any) => (
                     <TouchableOpacity key={radio.id} onPress={() => handleExamenPress(radio)}>
-                        <View className="bg-white rounded-xl mb-4 overflow-hidden shadow-sm">
-                            <View className="flex-row p-4">
-                                {/* Date Column */}
-                                <View className="items-center mr-4">
-                                    <Calendar size={24} color="#4F46E5" />
-                                    <Text className="text-xs font-medium mt-1">{new Date(radio.date).getDate()}</Text>
-                                    <Text className="text-xs">{new Date(radio.date).toLocaleString('default', { month: 'short' })}</Text>
-                                    <Text className="text-xs">{new Date(radio.date).getFullYear()}</Text>
-                                </View>
-
+                        <View className="bg-gray-50 rounded-xl mb-4 overflow-hidden shadow-sm">
                                 {/* Main Content */}
-                                <View className="flex-1">
+                                <View className="flex-1 px-4 py-4 mx-4">
                                     <Text className="font-semibold text-gray-900 mb-2">{radio.name}</Text>
+                                    <View className="flex-row justify-start">
+                                        <View className="items-center mr-4">
+                                            <Calendar size={24} color="#4F46E5" />
+                                            <Text className="font-bold text-lg font-medium mt-1">{new Date(radio.date).getDate()}</Text>
+                                            <Text className="font-bold text-lg">{new Date(radio.date).toLocaleString('default', { month: 'short' })}</Text>
+                                            {/*<Text className="text-xs">{new Date(radio.date).getFullYear()}</Text>*/}
+                                        </View>
+                                        {/* Laboratory Info */}
+                                      <View>
+                                          <View className="flex-row items-center mb-2">
+                                              <Building2 size={16} color="#666" />
+                                              <Text className="text-sm text-gray-600 ml-2">{radio.labName}</Text>
+                                          </View>
 
-                                    {/* Laboratory Info */}
-                                    <View className="flex-row items-center mb-2">
-                                        <Building2 size={16} color="#666" />
-                                        <Text className="text-sm text-gray-600 ml-2">{radio.labName}</Text>
-                                    </View>
-
-                                    {/* Doctor Info */}
-                                    <View className="flex-row items-center mb-3">
-                                        <User2 size={16} color="#666" />
-                                        <Text className="text-sm text-gray-600 ml-2">{radio.phone}</Text>
+                                          {/* Doctor Info */}
+                                          <View className="flex-row items-center mb-3">
+                                              <User2 size={16} color="#666" />
+                                              <Text className="text-sm text-gray-600 ml-2">{radio.phone}</Text>
+                                          </View>
+                                      </View>
                                     </View>
 
                                     {/* Action Buttons */}
-                                    <View className="flex-row justify-between">
+                                    <View className="flex-row gap-x-2 justify-end">
                                         <TouchableOpacity
-                                            className="bg-indigo-600 px-4 py-2 rounded-full"
+                                            className="bg-primary px-4 py-2 rounded-xl"
                                             onPress={() => handleExamenPress(radio)}
                                         >
                                             <Text className="text-white text-sm font-medium">Détail</Text>
                                         </TouchableOpacity>
 
                                         <TouchableOpacity
-                                            className="px-4 py-2 rounded-full border border-indigo-500"
+                                            className="px-4 py-2  border border-primary rounded-xl"
                                         >
                                             <Text className="text-sm font-medium text-indigo-500">
                                                 {radio?.uploads?.length ? `${radio.uploads.length} fichier(s)` : 'Aucun fichier'}
@@ -153,7 +153,6 @@ export default function ListRadiologie() {
                                         </TouchableOpacity>
                                     </View>
                                 </View>
-                            </View>
                         </View>
                     </TouchableOpacity>
                 ))}
