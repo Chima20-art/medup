a244a3688b0a0e6ba96fddb451082c8bf1504b34
Taoufiq Lotfi <admin@taoufiqlotfi.tech>
2025-02-18 23:26:49 +0200

    Add Apple login button to sign-in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,15 +6,17 @@ import {
   KeyboardAvoidingView,
   Platform,
   Animated,
-  StatusBar, SafeAreaView,
+  StatusBar,
+  SafeAreaView,
 } from "react-native";
 import { Link, Stack } from "expo-router";
 import { useTheme } from "@react-navigation/native";
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 import SignInForm from "@/components/SignInForm";
 import Logo from "@/assets/images/logo.svg";
 import Google from "@/assets/images/google.svg";
 import GoogleLoginButton from "@/components/googleLoginButton";
+import AppleLoginButton from "@/components/appleLoginButton";
 
 export default function Page() {
   const fadeAnim = React.useRef(new Animated.Value(0)).current;
@@ -29,62 +31,62 @@ export default function Page() {
   }, []);
 
   return (
-        <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={{ flex: 1 }}
-            className="p-10 pt-14"
-        >
-          <StatusBar barStyle="dark-content" />
-          <Stack.Screen
-              options={{
-                title: "Connexion",
-                headerShown: false,
-              }}
-          />
-          <Animated.ScrollView
-              contentContainerStyle={{ flexGrow: 1, justifyContent: "flex-start" }}
-              style={{ opacity: fadeAnim }}
-              showsVerticalScrollIndicator={false}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+      className="p-10 pt-14"
+    >
+      <StatusBar barStyle="dark-content" />
+      <Stack.Screen
+        options={{
+          title: "Connexion",
+          headerShown: false,
+        }}
+      />
+      <Animated.ScrollView
+        contentContainerStyle={{ flexGrow: 1, justifyContent: "flex-start" }}
+        style={{ opacity: fadeAnim }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={{ marginHorizontal: "auto" }}>
+          <Logo width={250} height={250} />
+        </View>
+
+        <View className="flex-row items-end justify-between mb-4 mt-10">
+          <Text
+            className="font-extrabold text-3xl"
+            style={{ color: colors.text }}
           >
-            <View style={{ marginHorizontal: "auto" }}>
-              <Logo width={250} height={250} />
-            </View>
+            Connexion
+          </Text>
+        </View>
 
-            <View className="flex-row items-end justify-between mb-4 mt-10">
-              <Text
-                  className="font-extrabold text-3xl"
-                  style={{ color: colors.text }}
-              >
-                Connexion
-              </Text>
-            </View>
+        <SignInForm />
 
-            <SignInForm />
-
-            <View className="flex-row justify-between mt-8 mb-4">
+        <View className="flex-row justify-between mt-8 mb-4">
+          <TouchableOpacity>
+            <Text className="text-sm" style={{ color: colors.text }}>
+              Besoin d'aide
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Link href="/sign-up-onboarding" asChild>
               <TouchableOpacity>
-                <Text className="text-sm" style={{ color: colors.text }}>
-                  Besoin d'aide
+                <Text className="text-base" style={{ color: colors.primary }}>
+                  S'inscrire
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity>
-                <Link href="/sign-up-onboarding" asChild>
-                  <TouchableOpacity>
-                    <Text className="text-base" style={{ color: colors.primary }}>
-                      S'inscrire
-                    </Text>
-                  </TouchableOpacity>
-                </Link>
-              </TouchableOpacity>
-            </View>
-            <View className="space-y-4">
-              <View className="bg-gray-400 h-[1px] w-full mb-4" />
-              <Text className="text-center text-gray-500 mb-4">Ou</Text>
+            </Link>
+          </TouchableOpacity>
+        </View>
+        <View className="space-y-4">
+          <View className="bg-gray-400 h-[1px] w-full mb-4" />
+          <Text className="text-center text-gray-500 mb-4">Ou</Text>
 
-              <GoogleLoginButton title=" Connexion avec google" />
-            </View>
-          </Animated.ScrollView>
-        </KeyboardAvoidingView>
+          <GoogleLoginButton title=" Connexion avec google" />
+          <AppleLoginButton />
+        </View>
+      </Animated.ScrollView>
+    </KeyboardAvoidingView>
   );
 }
-
